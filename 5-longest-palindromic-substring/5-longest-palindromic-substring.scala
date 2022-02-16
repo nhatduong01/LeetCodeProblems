af4bd8c909b66511c2eae5aaca364e30ee8dd15c
Nguyen Le Nhat Duong <58284222+nhatduong01@users.noreply.github.com>
2022-02-16 11:29:28 +0700

    Time: 1613 ms (40.91%), Space: 97.6 MB (28.18%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.scala
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.scala
@@ -1,29 +1,48 @@
 object Solution {
-  def checkPalindrome(s: String, low: Int, high: Int) : Boolean =
-    {
-      if(low == high || low > high)
-        true
-      else if (s(low) == s(high))
-         checkPalindrome(s,low +1, high -1)
-      else false
-    }
-    def longestPalindrome(s: String): String = 
-    {
-    var maxlength = 0;
-    var low = 0;
-    var high = 0;
-    for(index1 <- (0 to s.length -1))
-      {
-        for(index2 <- (index1 + maxlength  to s.length - 1))
-          {
-            if (checkPalindrome(s slice(index1, index2 +1), 0, index2 - index1) && (index2 + 1 - index1) > maxlength)
+
+  def longestPalindrome(s: String): String =
+{
+      var maxlength = 0;
+      var low = 0;
+      var high = 0;
+      var mytable = Array.ofDim[Boolean](s.length, s.length)
+      for( i <- 0 to s.length - 1)
+        {
+          mytable(i)(i) = true;
+        }
+      for(i <- 0 to s.length -2)
+        {
+          if(s(i) == s(i+1))
+            mytable(i)(i+1) = true
+        }
+      for(i <- (0 to s.length - 1).reverse)
+        {
+          for( j <- (i to s.length - 1).reverse)
             {
-              maxlength = index2 - index1 + 1
-              low = index1
-              high = index2
+              if(mytable(i)(j) == false)
+                {
+                  if(s(i) == s(j) && mytable(i+1)(j-1) == true)
+                    {
+                      mytable(i)(j) = true;
+                      if(j - i > maxlength)
+                        {
+                          low = i
+                          high = j
+                          maxlength = j - i
+                        }
+                    }
+                }
+              else
+                {
+                  if(j - i > maxlength)
+                  {
+                    low = i
+                    high = j
+                    maxlength = j - i
+                  }
+                }
             }
-          }
-      }
-    s.slice(low, high +1)
+        }
+      s.slice(low, high + 1)
     }
 }
